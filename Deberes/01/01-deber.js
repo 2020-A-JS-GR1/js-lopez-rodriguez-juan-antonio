96b679263345bc4aabd5aee1e3a0fb037b1f85a9
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-06-19 17:56:38 -0500

    Filter

---
+++ Deberes/01/01-deber.js
@@ -0,0 +1,61 @@
+/*DEBER para el viernes 26
+
+Utilizando el arreglo de la clase vamos a realizar las siguientes operaciones
+
+1) por cada vocal dentro de la palabra del estudiante 0.1 decimas
+2) por cada consonante dentro de la palabra del estudiante 0.05 decimas
+3) Filtrar cuales personas pasaron (los que sean >14)
+*/
+
+const arreglo = [
+    {
+        id:1,
+        nombre:'Juan',
+        nota:5
+    },
+    {
+        id:2,
+        nombre:'Antonio',
+        nota:8
+    },
+    {
+        id:3,
+        nombre:'Camila',
+        nota:9
+    },
+    {
+        id:4,
+        nombre:'Piki',
+        nota:13
+    },
+    {
+        id:5,
+        nombre:'Armando',
+        nota:15
+    },
+    {
+        id:6,
+        nombre:'Pepe',
+        nota:16
+    },
+    {
+        id:7,
+        nombre:'Marta',
+        nota:11
+    },
+    {
+        id:8,
+        nombre:'Pedro',
+        nota:18
+    },
+    {
+        id:9,
+        nombre:'Julio',
+        nota:17
+    },
+    {
+        id:10,
+        nombre:'Pablo',
+        nota:12
+    }
+];

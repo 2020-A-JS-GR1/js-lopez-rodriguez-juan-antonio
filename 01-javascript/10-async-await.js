55002c9027bd36ca335523eca636fdfd8d5e7152
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-07-07 17:04:19 -0500

    JS :)

--- 01-javascript/10-async-await.js
+++ 01-javascript/10-async-await.js
@@ -1,23 +1,50 @@
 
-const promesaLeerArchivo = new Promise();
-const promesaEscribirArchivo = new Promise();
+const promesaLeerArchivo = () => {
+    return new Promise(
+        (res, rej) => {
+            res('CONTENIDO LEER ARCHIVO')
+        }
+    );
+}
 
+const promesaEscribirArchivo = () => {
+    return new Promise(
+        (res, rej) => {
+            //res('CONTENIDO ESCRIBIR ARCHIVO')
+            rej('ERROR =( ')
+        }
+    );
+}
 //ASYNC AWAIT -> DENTRO DE UNA FUNCION
 async function ejercicio() {
     console.log('1');
     try{
-        const contenidoArchivo = await promesaLeerArchivo();
+        console.log('2');
+        const contenidoArchivoActual = await promesaLeerArchivo();
+        console.log(contenidoArchivoActual)
+        console.log('3');
         await promesaEscribirArchivo();
+        console.log('4');
         const nuevoContenido = await promesaLeerArchivo();
         console.log(nuevoContenido);
+        console.log('5');
     }catch (error) {
         console.error(error);
     }
+    console.log('6');
+    console.log('7');
 }
+const respuestaEjercicio = ejercicio();
+//console.log('respuestaEjercicio', respuestaEjercicio);
+//console.log('promesaLeerArchivo', promesaLeerArchivo());
+//console.log('promesaEscribirArchivo', promesaEscribirArchivo());
 
-const f = async () => {
+
+
+
+//const f = async () => {
     //contenido
-}
+
 
 
 

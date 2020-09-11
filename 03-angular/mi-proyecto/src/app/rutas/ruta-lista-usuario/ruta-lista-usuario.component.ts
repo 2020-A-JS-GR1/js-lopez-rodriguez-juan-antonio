3d48f89f46e612a76a4f07c1f27846a2dd9568bf
Juan Lopez <juan.lopez08@epn.edu.ec>
2020-09-11 17:59:25 -0500

    editar, eliminar usuarios, rutas

---
+++ 03-angular/mi-proyecto/src/app/rutas/ruta-lista-usuario/ruta-lista-usuario.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import {UsuarioService} from "../../servicios/http/usuario.service";
+import {Router} from "@angular/router";
+
+@Component({
+  selector: 'app-ruta-lista-usuario',
+  templateUrl: './ruta-lista-usuario.component.html',
+  styleUrls: ['./ruta-lista-usuario.component.css']
+})
+export class RutaListaUsuarioComponent implements OnInit {
+
+  arregloUsuarios = [];
+  constructor(
+    private readonly _usuarioService: UsuarioService,
+    private readonly _router: Router
+  ) {
+  }
+
+  irAEditarUsuario(id:number){
+    const ruta = ['/usuario', 'editar', id];
+    //usuario/editar/1
+    this._router.navigate(ruta);
+  }
+
+
+  eliminarUsuario(id: number) {
+    const obsEliminar = this._usuarioService
+      .eliminar(id);
+    obsEliminar
+      .subscribe(
+        () => {
+          // Borrando de la interfaz
+          const indice = this.arregloUsuarios
+            .findIndex(u => u.id === id);
+          this.arregloUsuarios.splice(indice, 1);
+        },
+        (error) => {
+          console.error('Error', error);
+        }
+      );
+  }
+
+  ngOnInit(): void {
+    const observableTraerTodos = this._usuarioService.traerTodos();
+    observableTraerTodos
+      .subscribe(
+        (usuarios: any[]) => {
+          this.arregloUsuarios = usuarios;
+        },
+        (error) => {
+          console.error('Error', error);
+        }
+      )
+  }
+
+}
--- 03-angular/mi-proyecto/src/app/servicios/http/usuario.service.ts
+++ 03-angular/mi-proyecto/src/app/servicios/http/usuario.service.ts
@@ -14,11 +14,21 @@ export class UsuarioService {
     return this._httpClient.get(this.url + '/Usuario')
   }
 
+  obtenerUnoPorId(idUsuario:number){
+    return this._httpClient.get(this.url + '/Usuario/' + idUsuario)
+  }
+
   //POST / Usuario
   crear(usuario){
     return this._httpClient.post(
       this.url + '/Usuario',   //URL
       usuario
+    )
+  }
+
+  eliminar(idUsuario:number){
+    return this._httpClient.delete(
+      this.url + '/Usuario/' + idUsuario
     )
   }
 
